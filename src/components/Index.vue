d2af142c3a5cb740fb0589d756ba173e635ba7a3
Preil <preil2000@gmail.com>
2019-07-26 11:36:29 +0300

    Firebase installed. Getting data from db implemented.

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -16,45 +16,59 @@
 </template>
 
 <script>
-export default {
-  name: 'Index',
-  data () {
-    return {
-      smoothies: [
-        {title: 'Ninja Brew', slug: 'ninja-brew', ingredients: ['bananas', 'coffee', 'milk'], id: 1},
-        {title: 'Morning mood', slug: 'morning-mood', ingredients: ['mango', 'lime', 'juice'], id: 2}
-      ]
-    }
-  },
-  methods: {
-    deleteSmoothie(id) {
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id != id
-      })
+  import db from '@/firebase/init'
+
+  export default {
+    name: 'Index',
+    data() {
+      return {
+        smoothies: []
+      }
+    },
+    methods: {
+      deleteSmoothie(id) {
+        this.smoothies = this.smoothies.filter(smoothie => {
+          return smoothie.id != id
+        })
+      }
+    },
+    created() {
+      // fetch data from firestore
+      db.collection('smoothies').get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            let smoothie = doc.data()
+            smoothie.id = doc.id
+            this.smoothies.push(smoothie)
+          })
+        })
     }
   }
-}
 </script>
 
 
-<style >
-  .index{
+<style>
+  .index {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     grid-gap: 30px;
     margin-top: 60px;
   }
-  .index h2{
+
+  .index h2 {
     font-size: 1.8em;
     text-align: center;
     margin-top: 0;
   }
-  .index .ingredients{
+
+  .index .ingredients {
     margin: 30px auto;
   }
-  .index .ingredients li{
+
+  .index .ingredients li {
     display: inline-block;
   }
+
   .index .delete {
     position: absolute;
     top: 4px;
